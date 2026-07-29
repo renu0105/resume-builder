92d313539f504dd78ad90aa5f17c2302f30918aa
renu0105 <renuchauhan0501@gmail.com>
2026-07-29 23:23:07 +0530

    update

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,9 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
